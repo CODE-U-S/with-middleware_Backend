75a95785fd13de5fba98db5d9359438c099daacf
hyeseung12 <peace10200khs@gmail.com>
2024-05-24 10:11:33 +0900

    init: 프로젝트 초기 환경 셋팅

---
+++ src/main/java/com/withmere/Withmere_Backend/WithmereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.withmere.Withmere_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithmereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithmereBackendApplication.class, args);
+	}
+
+}
