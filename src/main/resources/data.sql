ada7b3466377ffeb1ec3429977a15ea1a5f7d762
darecoco <66424222+darecoco@users.noreply.github.com>
2024-05-28 13:24:36 +0900

    초기값 PK 중복 삽입 해결

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (nickname, user_comment, email, password, created_date, category) VALUES ('김혜승', 'hyeseung', 's2224@e-mirim.hs.kr', 'hyeseung1!', '2023-10-20 10:11:20', 1);
+REPLACE INTO users (nickname, user_comment, email, password, created_date, category) VALUES ('김혜승', 'hyeseung', 's2224@e-mirim.hs.kr', 'hyeseung1!', '2023-10-20 10:11:20', 1);
