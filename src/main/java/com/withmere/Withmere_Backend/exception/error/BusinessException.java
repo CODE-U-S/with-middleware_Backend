f57b7c8137f186bdab768593c40b7852210de562
MsEmily1020 <peace10200khs@gmail.com>
2024-05-29 09:50:07 +0900

    feat: BusinessException 추가

---
+++ src/main/java/com/withmere/Withmere_Backend/exception/error/BusinessException.java
@@ -0,0 +1,10 @@
+package com.withmere.Withmere_Backend.exception.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+}
