05d745763a49b0c542e8bdea97602d72463fd5b4
MsEmily1020 <peace10200khs@gmail.com>
2024-05-29 09:48:58 +0900

    fix: errorcode enum , 수정

--- src/main/java/com/withmere/Withmere_Backend/exception/error/ErrorCode.java
+++ src/main/java/com/withmere/Withmere_Backend/exception/error/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     FOLLOW_DUPLICATE(HttpStatus.CONFLICT, "Follow Duplicate"), // 유저의 이메일이 중복됨
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User Not Found"), // 유저를 찾을 수 없음
-    FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "Follow Not Found") // 팔로우한 기록을 찾을 수 없음
+    FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "Follow Not Found"), // 팔로우한 기록을 찾을 수 없음
 
     private final HttpStatus status;
     private final String message;
