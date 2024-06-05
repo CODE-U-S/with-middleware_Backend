98cd4d8c41fe83ec32fe327f627c42ddc9723c11
darecoco <66424222+darecoco@users.noreply.github.com>
2024-06-06 01:39:56 +0900

    post더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 REPLACE INTO users (nickname, user_comment, email, password, created_date, category) VALUES ('김혜승', 'rlagPtmd', 'rlagPtmd@e-mirim.hs.kr', 'rlagPtmd1!', '2023-10-20 10:11:20', 1);
 REPLACE INTO users (nickname, user_comment, email, password, created_date, category) VALUES ('이진선', 'dlwlstjs', 'dlwlstjs@e-mirim.hs.kr', 'dlwlstjs1!', '2023-10-20 10:11:20', 1);
 REPLACE INTO users (nickname, user_comment, email, password, created_date, category) VALUES ('이지인', 'dlwlsdls', 'dlwldls@e-mirim.hs.kr', 'dlwlsdls1!', '2023-10-20 10:11:20', 1);
+REPLACE INTO post (email) VALUE ('dlwldls@e-mirim.hs.kr');
