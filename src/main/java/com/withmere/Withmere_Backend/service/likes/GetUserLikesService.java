7f09a771488d0f0f81cdc634a7449501e937975f
darecoco <66424222+darecoco@users.noreply.github.com>
2024-06-06 00:18:28 +0900

    Add Get:UserLikes

---
+++ src/main/java/com/withmere/Withmere_Backend/service/likes/GetUserLikesService.java
@@ -0,0 +1,30 @@
+package com.withmere.Withmere_Backend.service.likes;
+
+import com.withmere.Withmere_Backend.domain.user.User;
+import com.withmere.Withmere_Backend.dto.likes.LikesResponse;
+import com.withmere.Withmere_Backend.exception.UserNotFoundException;
+import com.withmere.Withmere_Backend.repository.LikesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class GetUserLikesService {
+    private final LikesRepository lr;
+
+    @Transactional
+    public List<LikesResponse> execute(User user){
+        if(user == null) throw UserNotFoundException.EXCEPTION;
+
+        List<LikesResponse> userLikes = lr.showAllUserLikes(user)
+                .stream()
+                .map(LikesResponse::new)
+                .collect(Collectors.toList());
+
+        return userLikes;
+    }
+}
